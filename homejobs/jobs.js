cad3cc12dd2d1438e662943160059f5f65592ef0
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-11-15 21:52:16 +0100

    Testing some es6

--- homejobs/jobs.js
+++ homejobs/jobs.js
@@ -1,8 +1,19 @@
 
-const arreySum= (x,y,z) =>{
-let result= x+y+z
+// const numebrs= [2,3,4]
 
-return result
+// const sum= numebrs.reduce((total, currentValue) => total+currentValue); // riduce ad unico valore l'array
+// console.log(sum)
+
+
+
+// const filter= numebrs.filter((x)=>{return x>=3}) // ritorna tutto quello che soddifa la condizione mantendo quelli che non lo soddifano 
+
+// console.log(filter)
+
+// const map= numebrs.map((x)=>{return x*2}) // per la lunghezza dell'array fa qualcosa che io decido 
+// console.log(map)
+
+const contains= (arr)=>{
+return arr.includes(1|3)
 }
-
-console.log(arreySum(2,4,5))
+console.log(contains([2,4]))
